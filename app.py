f8a1dce280e02480626016133c1d7cc5f7e78fc7
rupam-iics <rupam.iics@gmail.com>
2016-09-16 16:35:33 +0530

    copied runserver.py as app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+This script runs the FlaskWebProject1 application using a development server.
+"""
+
+from os import environ
+from ECG import app
+from waitress import serve
+import os
+
+if __name__ == '__main__':
+     
+        #PORT=int(os.environ.get('PORT', 5000))
+        PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
+        #serve(app, port=PORT)
+        serve(app, port=PORT)
+        #set this command in heroku command
+        #heroku config:add PORT=5000
